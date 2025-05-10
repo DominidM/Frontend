e3b9c7bf2be4060c0a11b4423fef19dc97251cb5
Dominid Muñoz Eslava <dominidzero@gmail.com>
2025-05-09 23:42:47 -0500

    update 5
    prueba

--- frontend/src/pages/CartPage.tsx
+++ frontend/src/pages/CartPage.tsx
@@ -1,4 +1,5 @@
 //import { Outlet } from 'react-router-dom';
+//update actulizacion para prueba;
 //import React from 'react';
 import { Publicidad } from '../components/Publicidad';
 import { Hero } from "../components/Hero";
